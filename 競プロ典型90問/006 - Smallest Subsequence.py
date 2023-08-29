84685d53fc1f85e3b2ced5108ab3713d712058a0
toshiki19 <toshiki.u.19@gmail.com>
2023-08-29 18:17:44 +0900

    答えを返す

--- 競プロ典型90問/006 - Smallest Subsequence.py
+++ 競プロ典型90問/006 - Smallest Subsequence.py
@@ -5,3 +5,15 @@ def calc_next(S):
     N = len(S)
     # 答え
     res = [[N] * 26 for _ in range(N + 1)]
+    
+    # 後ろから見る
+    for i in range(N - 1, -1, -1):
+        # i + 1 文字目以降の結果を一旦 i 文字にコピー
+        for j in range(26):
+            res[i][j] = res[i + 1][j]
+
+        # i 文字目の情報を反映させる
+        res[i][ord(S[i]) - ord('a')] = i
+
+    # 答えを返す
+    return res
